2f306a5e2ae79420aedbc5b97804d239aa110bf2
Suchet Shenoy <suchet4learning@gmail.com>
2022-01-24 22:46:01 +1100

    Java practice programs

--- FirstJavaProject/src/com/january/DectoBin.java
+++ FirstJavaProject/src/com/january/DectoBin.java
@@ -1,8 +1,22 @@
 package com.january;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DectoBin {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Sum = 0, n = sc.nextInt();
+        ArrayList<Integer> Count = new ArrayList<>();
 
+        for (int i=0; i < n; i++) {
+            Count.add(sc.nextInt());
+        }
+
+        for (int i : Count) {
+            Sum = Sum + i;
+        }
+        System.out.println(Sum);
     }
 }
