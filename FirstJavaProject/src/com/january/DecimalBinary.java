3f2b0c83c6c5603a39f0d5603bf299ed96876bb7
Suchet Shenoy <suchet4learning@gmail.com>
2022-01-25 21:58:04 +1100

    Java practice programs

--- FirstJavaProject/src/com/january/DecimalBinary.java
+++ FirstJavaProject/src/com/january/DecimalBinary.java
@@ -1,23 +1,16 @@
 package com.january;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DecimalBinary {
+    public static int Decbin(int n) {
+        if (n==0) return 0;
+        return (Decbin(n/2) * 10 + (n %2));
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> A = new ArrayList<>();
         int n = sc.nextInt();
-        int Num = 0;
-        int Bin = 0, Rem = 0;
-        for (int i=0; n > 0; i++) {
-            A.add((n % 2));
-            n = n / 2;
-        }
-        for (int i = A.size()-1; i>=0; i--) {
-            Bin = (Bin * 10) + A.get(i);
-        }
-        System.out.println(Bin);
+        System.out.println(Decbin(n));
     }
 }
--- FirstJavaProject/src/com/january/Solution.java
+++ FirstJavaProject/src/com/january/Solution.java
@@ -2,14 +2,15 @@ package com.january;
 
 import java.util.Scanner;
 
-public class MagicNum {
-    public static int Decbin(int n) {
-        if (n==0) return 0;
-        return ((Decbin(n/2) * 5 + (n %2) * 5));
+public class Solution {
+    public static int solve(int A) {
+        if (A==0) return 0;
+        return ((solve(A/2) * 5 + (A%2) * 5));
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(Decbin(n));
+        System.out.println(solve(n));
     }
 }
