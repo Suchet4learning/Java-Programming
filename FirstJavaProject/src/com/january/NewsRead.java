dfa343db9e9125a2b0e5a5d8b2a49298256d422f
Suchet Shenoy <suchet4learning@gmail.com>
2022-01-27 23:22:29 +1100

    Java practice programs

--- FirstJavaProject/src/com/january/NewsRead.java
+++ FirstJavaProject/src/com/january/NewsRead.java
@@ -1,40 +1,51 @@
 package com.january;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NewsRead {
-    public static int solve(int A, ArrayList<Integer> B) {
-        int Sum = 0, i = 0;
-        while (i<B.size())  {
-            Sum = Sum + B.get(i);
-            i++;
-            if (Sum >= A) break;
-            if (i==B.size()) {
-                if (A % Sum !=0) {
-                        i = 0;
-                        A = A % Sum;
-                        Sum = 0;
-                }
-                else if (Sum == 1) {
-                    i = 0;
-                    A = A - Sum;
-                }
-                else break;
+
+    public int solve(int A, ArrayList<Integer> B) {
+        int sum = 0, i;
+        for (i=0; i<B.size(); i++) {
+            sum = sum + B.get(i);
+            if (sum >= A) {
+                return (i+1);
             }
         }
-        return (i);
+        if (sum > 1) {
+            if ((A % sum)==0) {
+               A = sum;
+            }
+            else {
+                A = (A % sum);
+            }
+            sum = 0;
+        }
+        else {
+            sum = ((A-1) / sum);
+        }
+        for (i=0; i<B.size(); i++) {
+            sum = sum + B.get(i);
+            if (sum >= A) {
+                return (i+1);
+            }
+        }
+        return (i + 1);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int i = sc.nextInt();
         ArrayList<Integer> Arr = new ArrayList<>();
+        int Sum =0;
+        NewsRead obj = new NewsRead();
 
         for (int j=0; j<i; j++) {
             Arr.add(sc.nextInt());
         }
-        System.out.println(solve(n,Arr));
-
+        System.out.println(obj.solve(n,Arr));
     }
 }
