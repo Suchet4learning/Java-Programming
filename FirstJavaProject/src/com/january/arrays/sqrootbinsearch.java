b07a156e603fe4dfac93555878eed361a3b9f6ed
Suchet Shenoy <suchet4learning@gmail.com>
2022-01-30 15:04:46 +1100

    Java practice programs

--- FirstJavaProject/src/com/january/arrays/sqrootbinsearch.java
+++ FirstJavaProject/src/com/january/arrays/sqrootbinsearch.java
@@ -1,4 +1,29 @@
 package com.january.arrays;
 
+import java.util.Scanner;
+
 public class sqrootbinsearch {
+
+    static int sqrtbin(int n) {
+        int s = 0, e = n, i = 1;
+        int mid, ans = 0;
+
+        while (s<=e) {
+            mid = (s+e)/2;
+            if (mid*mid<=n) {
+                ans = mid;
+                s = mid + 1;
+            }
+            else {
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sqrtbin(n));
+    }
 }
