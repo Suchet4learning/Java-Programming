5edf832e01b41004ad79a60549a3330202ff9b7f
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-02 23:00:48 +1100

    Java practice programs

---
+++ FirstJavaProject/src/com/february/PrintOdd.java
@@ -0,0 +1,17 @@
+package com.february;
+
+import java.util.Scanner;
+
+public class PrintOdd {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i= 1; i<=n; i++) {
+            if ((i % 2) !=0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
