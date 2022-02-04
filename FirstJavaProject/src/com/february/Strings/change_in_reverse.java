c6f91b53001d295363e63cc6193710127db64526
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-04 13:33:47 +1100

    Java practice programs

---
+++ FirstJavaProject/src/com/february/Strings/change_in_reverse.java
@@ -0,0 +1,30 @@
+package com.february.Strings;
+
+import java.util.Scanner;
+
+public class change_in_reverse {
+
+    public int countRev(String A) {
+        String Revs = "";
+        int diff=0;
+        for (int i=A.length()-1; i>=0; i--) {
+            Revs = Revs + A.charAt(i);
+        }
+        for (int i=0; i<A.length(); i++) {
+            if (A.charAt(i) != Revs.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        change_in_reverse obj = new change_in_reverse();
+        System.out.println(obj.countRev(s));
+
+    }
+}
