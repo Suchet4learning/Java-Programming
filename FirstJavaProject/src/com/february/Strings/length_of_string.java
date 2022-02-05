75a81a7959478453a1ffa3b81db7db3a769e79ba
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-05 17:17:06 +1100

    Java practice programs

--- FirstJavaProject/src/com/february/Strings/length_of_string.java
+++ FirstJavaProject/src/com/february/Strings/length_of_string.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class length_of_string {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
+        for (int i=0; i<T; i++) {
+            System.out.println(sc.next().length());
+        }
     }
 }
