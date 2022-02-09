9b68db7ebb7975c471060669758d9156f93ca2b1
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-09 23:47:27 +1100

    Java practice programs

---
+++ FirstJavaProject/src/com/february/exercises/gcd_without_recursion.java
@@ -0,0 +1,23 @@
+package com.february.exercises;
+
+import java.util.Scanner;
+
+public class gcd_without_recursion {
+
+    public static int gcd(int Dividend, int Divisor) {
+        int Rem = Dividend % Divisor;
+        while(Rem > 0) {
+            Dividend = Divisor;
+            Divisor = Rem;
+            Rem = Dividend % Divisor;
+        }
+        return Divisor;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Divisor = sc.nextInt();
+        int Dividend = sc.nextInt();
+        System.out.println(gcd(Dividend,Divisor));
+    }
+}
