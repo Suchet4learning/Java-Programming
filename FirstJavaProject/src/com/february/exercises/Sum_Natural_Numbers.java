fc02f0dd844692127904b304a98fede39476739e
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-16 10:57:13 +1100

    Java practice programs

---
+++ FirstJavaProject/src/com/february/exercises/Sum_Natural_Numbers.java
@@ -0,0 +1,12 @@
+package com.february.exercises;
+
+import java.util.Scanner;
+
+public class Sum_Natural_Numbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * (n + 1) / 2);
+    }
+}
