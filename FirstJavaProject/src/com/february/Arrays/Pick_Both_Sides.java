a2cb73ed73fa4b583848fcfa76f8e3a6ace8d71d
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-26 14:29:58 +1100

    Java practice programs

---
+++ FirstJavaProject/src/com/february/Arrays/Pick_Both_Sides.java
@@ -0,0 +1,36 @@
+package com.february.Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Pick_Both_Sides {
+
+    public int solve(ArrayList<Integer> a, int b) {
+
+        for (int i=1; i<a.size(); i++) {
+            a.set(i, a.get(i-1)+a.get(i));
+        }
+
+        int max = a.get(b-1);
+        if (a.get((a.size()-1)-(b-1)) > max) {
+            max = a.get((a.size() - 1) - (b - 1));
+        }
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt(), B;
+        ArrayList<Integer> A = new ArrayList<>();
+        Pick_Both_Sides obj = new Pick_Both_Sides();
+
+        for (int i=0; i<n; i++) {
+            A.add(sc.nextInt());
+        }
+
+        B = sc.nextInt();
+        System.out.println(obj.solve(A,B));
+    }
+}
