20bb23b1326dd59d04171e9e7a6cffeb9cde20a2
Suchet Shenoy <suchet4learning@gmail.com>
2022-02-28 14:12:01 +1100

    Java practice programs

--- FirstJavaProject/src/com/february/Arrays/Bulbs.java
+++ FirstJavaProject/src/com/february/Arrays/Bulbs.java
@@ -1,4 +1,40 @@
 package com.february.Arrays;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Bulbs {
+
+    public int bulbs(ArrayList<Integer> A) {
+
+        int ans = 0;
+
+        for (int i=0; i<A.size();i++) {
+            if (ans % 2 == 0) {
+                if (A.get(i) == 0) {
+                    ans++;
+                }
+            } else {
+                if(A.get(i) == 1) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> a = new ArrayList<>();
+        Bulbs obj = new Bulbs();
+
+        for (int i=0; i<n; i++) {
+            a.add(sc.nextInt());
+        }
+
+        System.out.println(obj.bulbs(a));
+    }
+
 }
