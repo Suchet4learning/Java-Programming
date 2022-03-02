6656aa315f053d7ed9bfa9803fa76bd3ef2ba057
Suchet Shenoy <suchet4learning@gmail.com>
2022-03-03 01:16:24 +1100

    Java practice programs

--- FirstJavaProject/src/com/March/Arrays/Max_Sum_Subarray.java
+++ FirstJavaProject/src/com/March/Arrays/Max_Sum_Subarray.java
@@ -2,6 +2,8 @@ package com.March.Arrays;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Max_Sum_Subarray {
 
@@ -23,6 +25,8 @@ public class Max_Sum_Subarray {
 
     public static void main(String[] args) {
 
+        SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss.SSS");
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         ArrayList<Integer> a = new ArrayList<>();
@@ -31,6 +35,12 @@ public class Max_Sum_Subarray {
         for (int i=0; i<n; i++) {
             a.add(sc.nextInt());
         }
+
+        Date dt = new Date();
+        System.out.println(fmt.format(dt));
         System.out.println(obj.maxSubArray(a));
+        dt = new Date();
+        System.out.println(fmt.format(dt));
+
     }
 }
