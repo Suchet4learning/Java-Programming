6102bb51409d84bd64ccb537e86dfed378945c65
Suchet Shenoy <suchet4learning@gmail.com>
2022-03-20 21:34:55 +1100

    Java practice programs

---
+++ FirstJavaProject/src/com/March/Day35/Mod_String.java
@@ -0,0 +1,62 @@
+/*
+Problem Description
+You are given a large number in the form of a string A where each character denotes a digit of the number.
+You are also given a number B. You have to find out the value of A % B and return it.
+
+Problem Constraints
+1 <= A.length() <= 105
+0 <= Ai <= 9
+1 <= B <= 109
+
+Input Format
+The first argument is a string A.
+The second argument is an integer B.
+
+Output Format
+Return a single integer denoting the value of A % B.
+
+Example Input
+Input 1:
+A = "143"
+B = 2
+Input 2:
+A = "43535321"
+B = 47
+
+Example Output
+Output 1:
+1
+Output 2:
+20
+
+Example Explanation
+Explanation 1:
+143 is an odd number so 143 % 2 = 1.
+Explanation 2:
+43535321 % 47 = 20
+ */
+package com.March.Day35;
+
+import java.util.Scanner;
+
+public class Mod_String {
+
+    public int findMod(String A, int B) {
+
+        Long ans=0L;
+        Long n = 1L;
+        for (int i=A.length()-1; i>=0; i--) {
+            ans = (ans % B + (long)(A.charAt(i) - '0') % B * n % B) % B;
+            n = (n * 10) % B;
+        }
+        return ans.intValue();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = sc.nextInt();
+        Mod_String obj = new Mod_String();
+        System.out.println(obj.findMod(s,n));
+    }
+}
